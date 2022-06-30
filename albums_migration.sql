033e2d06ea700528600e6c555d2d5fcd762184cb
Mark Siebald <siebald.mark93@gmail.com>
2022-06-29 19:25:45 -0500

    feat:Albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,12 +1,13 @@
 USE codeup_test_db;
 
-    DROP TABLE IF EXISTS;
+    DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL Auto_increment,
-    artist VARCHAR(50),
-    name VARCHAR(50),
+    artist VARCHAR(200) NOT NULL ,
+    name VARCHAR(200) NOT NULL,
     release_date INT,
     sales float,
-    genre VARCHAR(50)
+    genre VARCHAR(200),
+    PRIMARY KEY(id)
 );
