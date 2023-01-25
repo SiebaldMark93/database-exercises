f01c53a2d2957ddb57012fd4fee4e7673234cf48
Mark Siebald <Siebald.Mark93@gmail.com>
2023-01-25 14:29:56 -0600

    delete exercise

--- delete_exercise.sql
+++ delete_exercise.sql
@@ -3,19 +3,18 @@ USE codeup_test_db;
 SELECT * FROM albums
 WHERE release_date > 1991;
 
-DELETE * FROM albums
+DELETE  FROM albums
 WHERE release_date > 1991;
 
 SELECT * FROM albums
 WHERE genre = 'disco';
 
-DELETE * FROM albums
-WHERE genre = 'disco';
+DELETE  FROM albums WHERE genre = 'disco';
 
 
 SELECT * FROM albums
 WHERE artist = 'Whitney Houston/ Various Artists';
 
 
-DELETE * FROM albums
+DELETE  FROM albums
 WHERE artist = 'Whitney Houston/ Various Artists';
