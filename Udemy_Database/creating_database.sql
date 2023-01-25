01623c7acaca52e8d63f53e379c4520f49584d1e
Mark Siebald <Siebald.Mark93@gmail.com>
2023-01-25 14:47:38 -0600

    feat: practice creating a database and deleting

---
+++ Udemy_Database/creating_database.sql
@@ -0,0 +1,23 @@
+SHOW DATABASES;
+
+# CREATE DATABASE
+CREATE DATABASE test;
+
+# To manipulate or use a database use "USE"
+USE test;
+
+# To see tables inside test database
+SHOW TABLES;
+
+# To delete a database use the DROP;
+DROP DATABASE test;
+
+# Show databases to verify database was deleted.
+SHOW DATABASES;
+
+SHOW DATABASES;
+CREATE DATABASE test2;
+USE test2;
+SHOW TABLES;
+DROP DATABASE test2;
+SHOW DATABASES;
